09b573f0c6281c650a47f629e48b238da9bcad89
nguyentronglinh045 <nguyentronglinh045@gmail.com>
2023-09-04 16:32:27 +0700

    config tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const plugin = require('tailwindcss/plugin')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {}
+  corePlugins: {
+    container: false
   },
-  plugins: []
+  theme: {
+    extend: {
+      colors: {
+        'main-color': '#d70018'
+      }
+    }
+  },
+  plugins: [
+    plugin(function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          maxWidth: '1370px',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          paddingLeft: '10px',
+          paddingRight: '10px'
+        }
+      })
+    })
+  ]
 }
