209c5f590ec72b3d3bf755588bd78b220feb8a99
[huytoan] <huytoannguyen717@gmail.com>
2023-09-08 15:37:07 +0700

    Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,89 @@
+import FooterLogo from 'src/assets/images/logo_foo.webp'
+import FacebookIcon from 'src/assets/icons/facebook.webp'
+import TwitterIcon from 'src/assets/icons/twitter.webp'
+import InstagramIcon from 'src/assets/icons/instagram.webp'
+import YoutubeIcon from 'src/assets/icons/youtube.webp'
+import ShoppeIcon from 'src/assets/icons/shopee.webp'
+import LazadaIcon from 'src/assets/icons/lazada.webp'
+import Payment4Img from 'src/assets/images/payment-4.webp'
+import Payment3Img from 'src/assets/images/payment-3.webp'
+import Payment2Img from 'src/assets/images/payment-2.webp'
+import Payment1Img from 'src/assets/images/payment-1.webp'
+
+import { productsType } from 'src/constants'
+
 export default function Footer() {
-  return <div>Footer</div>
+  return (
+    <div className='flex flex-col'>
+      <div className='flex flex-wrap max-sm:flex-col max-md:gap-y-4'>
+        <div className='px-2 md:w-1/2'>
+          <img src={FooterLogo} alt='footer logo' className='max-w-full h-[36px]' title='Meo Mobile' />
+          <div className='flex gap-1'>
+            <h3 className='font-bold'>Trụ sở chính: </h3>
+            <p className='text-[14px] text-[#4c4c4c]'>Ladeco Building, 266 Doi Can Street, Ba Dinh District, Hanoi.</p>
+          </div>
+          <div className='flex gap-1'>
+            <h3 className='font-bold'>Email: </h3>
+            <p className='text-[14px] text-[#4c4c4c]'>support@sapo.vn</p>
+          </div>
+          <div className='flex gap-1'>
+            <h3 className='font-bold'>Hotline: </h3>
+            <p className='text-[14px] text-[#4c4c4c]'>1900 6750</p>
+          </div>
+          <p className='text-[14px] text-[#4c4c4c]'>
+            Giấy chứng nhận Đăng ký Kinh doanh số 0309532xxx do Sở Kế hoạch và Đầu tư Thành phố Hà Nội cấp ngày
+            01/04/2002
+          </p>
+        </div>
+        <div className='px-2 md:w-1/4 sm:w-1/2'>
+          <h1 className='font-bold text-[20px] text-[#4c4c4c]'>Chính sách</h1>
+          <ul className='mt-3'>
+            <li>
+              <a className='hover:text-[#d70018] text-[14px] text-[#4c4c4c]' href='/'>Chính sách mua hàng</a>
+            </li>
+            <li>
+              <a className='hover:text-[#d70018] text-[14px] text-[#4c4c4c]' href='/'>Chính sách đổi trả</a>
+            </li>
+            <li>
+              <a className='hover:text-[#d70018] text-[14px] text-[#4c4c4c]' href='/'>Chính sách bảo hành</a>
+            </li>
+            <li>
+              <a className='hover:text-[#d70018] text-[14px] text-[#4c4c4c]' href='/'>Gửi góp ý, khiếu nại</a>
+            </li>
+          </ul>
+        </div>
+        <div className='px-2 md:w-1/4 sm:w-1/2'>
+          <h1 className='font-bold text-[20px] text-[#4c4c4c]'>Kết nối với chúng tôi</h1>
+          <div className='flex flex-wrap gap-2 mt-2'>
+            <img src={FacebookIcon} alt='facebook' className='w-[32px] h-[32px] rounded-full' />
+            <img src={TwitterIcon} alt='twitter' className='w-[32px] h-[32px] rounded-full' />
+            <img src={InstagramIcon} alt='instagram' className='w-[32px] h-[32px] rounded-full' />
+            <img src={YoutubeIcon} alt='youtube' className='w-[32px] h-[32px] rounded-full' />
+            <img src={ShoppeIcon} alt='shoppe' className='w-[32px] h-[32px] rounded-full' />
+            <img src={LazadaIcon} alt='lazada' className='w-[32px] h-[32px] rounded-full' />
+          </div>
+          <p className='mt-4'>Phương thức thanh toán</p>
+          <div className='flex flex-wrap gap-2 mt-2'>
+            <img src={Payment1Img} alt='payment' className='w-[45px] h-[30px]' />
+            <img src={Payment2Img} alt='payment' className='w-[45px] h-[30px]' />
+            <img src={Payment3Img} alt='payment' className='w-[45px] h-[30px]' />
+            <img src={Payment4Img} alt='payment' className='w-[45px] h-[30px]' />
+          </div>
+        </div>
+      </div>
+      <div className='w-full h-[1px] bg-slate-300 my-8' />
+      <ul className='flex flex-wrap'>
+        {productsType.map((product) => (
+          <li
+            key={product.id}
+            className='max-sm:w-1/2 sm:w-1/4 max-md:w-1/6'
+          >
+            <a className='hover:text-[#d70018] text-[14px] text-[#4c4c4c]' href={product.link}>{product.name}</a>
+          </li>
+        ))}
+      </ul>
+      <div className='w-full h-[1px] bg-slate-300 my-8' />
+      <p className='text-center'>Copyright&copy;: Ahihi đồ ngốc</p>
+    </div>
+  )
 }
---
+++ src/constants/index.tsx
@@ -0,0 +1,97 @@
+export const productsType = [
+  {
+    id: 1,
+    name: 'Điện thoại',
+    link: '/'
+  },
+  {
+    id: 2,
+    name: 'Laptop',
+    link: '/'
+  },
+  {
+    id: 3,
+    name: 'Thiết bị mạng',
+    link: '/'
+  },
+  {
+    id: 4,
+    name: 'Tai nghe',
+    link: '/'
+  },
+  {
+    id: 5,
+    name: 'Loa',
+    link: '/'
+  },
+  {
+    id: 6,
+    name: 'Cáp, củ sạc',
+    link: '/'
+  },
+  {
+    id: 7,
+    name: 'Chuột',
+    link: '/'
+  },
+  {
+    id: 8,
+    name: 'Phụ kiện gaming',
+    link: '/'
+  },
+  {
+    id: 9,
+    name: 'Máy in',
+    link: '/'
+  },
+  {
+    id: 10,
+    name: 'Thiết bị nhà thông minh',
+    link: '/'
+  },
+  {
+    id: 11,
+    name: 'Smart watch',
+    link: '/'
+  },
+  {
+    id: 12,
+    name: 'Máy tính bảng',
+    link: '/'
+  },
+  {
+    id: 13,
+    name: 'Tivi',
+    link: '/'
+  },
+  {
+    id: 14,
+    name: 'Ốp lưng, bao da',
+    link: '/'
+  },
+  {
+    id: 15,
+    name: 'Pin dự phòng',
+    link: '/'
+  },
+  {
+    id: 16,
+    name: 'Màn hình',
+    link: '/'
+  },
+  {
+    id: 17,
+    name: 'Bàn phím',
+    link: '/'
+  },
+  {
+    id: 18,
+    name: 'Camera, webcam',
+    link: '/'
+  },
+  {
+    id: 19,
+    name: 'Máy quét mã vạch',
+    link: '/'
+  }
+]
