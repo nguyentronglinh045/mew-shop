109df7bb6124816b7e8ae2fcc8a75e2927620864
nguyentronglinh045 <nguyentronglinh045@gmail.com>
2023-09-14 17:17:58 +0700

    show error not 422 and 401 in register

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,4 +1,6 @@
-import axios, { type AxiosInstance } from 'axios'
+import axios, { AxiosError, HttpStatusCode, type AxiosInstance } from 'axios'
+import { toast } from 'react-toastify'
+
 const apiKey = import.meta.env.VITE_API_KEY
 
 class Http {
@@ -9,6 +11,28 @@ class Http {
       timeout: 10000,
       headers: { 'Content-Type': 'application/json' }
     })
+    this.instance.interceptors.response.use(
+      (response) => {
+        // Bất kì mã trạng thái nào nằm trong tầm 2xx đều khiến hàm này được trigger
+        // Làm gì đó với dữ liệu response
+        return response
+      },
+      (error: AxiosError) => {
+        // Bất kì mã trạng thái nào lọt ra ngoài tầm 2xx đều khiến hàm này được trigger\
+        // Làm gì đó với lỗi response
+
+        // Chỉ Toast lỗi không phải 422 và 401
+        if (
+          ![HttpStatusCode.UnprocessableEntity, HttpStatusCode.Unauthorized].includes(error.response?.status as number)
+        ) {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          const data: any | undefined = error.response?.data
+          const message = data.message || error.message
+          toast.error(message)
+        }
+        return Promise.reject(error)
+      }
+    )
   }
 }
 
