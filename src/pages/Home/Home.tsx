bc48c2f3bcba725733e69cff14f4f3d2eb9884a9
nguyentronglinh045 <nguyentronglinh045@gmail.com>
2023-09-14 18:28:35 +0700

    create Home component

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import FeaturedCategories from 'src/components/FeaturedCategories'
+import SliderBanner from 'src/components/SliderBanner/SliderBanner'
+
+export default function Home() {
+  return (
+    <>
+      <SliderBanner />
+      <FeaturedCategories />
+    </>
+  )
+}
