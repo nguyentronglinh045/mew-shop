b6427b581587d8ceb8047583eb59f90e02d4a8a3
nguyentronglinh045 <nguyentronglinh045@gmail.com>
2023-09-14 18:29:10 +0700

    update route
    update route, create ProtectedRoute and RejectedRoute

--- src/useRouteElement.tsx
+++ src/useRouteElement.tsx
@@ -1,11 +1,21 @@
-import { useRoutes } from 'react-router-dom'
+import { Navigate, Outlet, useRoutes } from 'react-router-dom'
 import { Suspense, lazy } from 'react'
 import MainLayout from './layouts/MainLayout'
 import path from './constants/path.ts'
 
+const Home = lazy(() => import('./pages/Home'))
 const NotFound = lazy(() => import('./pages/NotFound'))
 const Login = lazy(() => import('./pages/Login'))
 const Register = lazy(() => import('./pages/Register'))
+const Profile = lazy(() => import('./pages/User/Profile/index.ts'))
+
+const isAuthenticated = false
+function ProtectedRoute() {
+  return isAuthenticated ? <Outlet /> : <Navigate to={path.login} />
+}
+function RejectedRoute() {
+  return !isAuthenticated ? <Outlet /> : <Navigate to={path.home} />
+}
 
 export default function useRouteElement() {
   const routeElements = useRoutes([
@@ -20,23 +30,55 @@ export default function useRouteElement() {
     {
       path: '',
       index: true,
-      element: <MainLayout />
-    },
-    {
-      path: path.login,
       element: (
-        <Suspense>
-          <Login />
+        <Suspense fallback={<div>Loading...</div>}>
+          <MainLayout>
+            <Home />
+          </MainLayout>
         </Suspense>
       )
     },
     {
-      path: path.register,
-      element: (
-        <Suspense>
-          <Register />
-        </Suspense>
-      )
+      path: '',
+      element: <RejectedRoute />,
+      children: [
+        {
+          path: path.login,
+          element: (
+            <Suspense>
+              <Login />
+            </Suspense>
+          )
+        },
+        {
+          path: path.register,
+          element: (
+            <Suspense>
+              <Register />
+            </Suspense>
+          )
+        }
+      ]
+    },
+    {
+      path: '',
+      element: <ProtectedRoute />,
+      children: [
+        {
+          path: path.user,
+          element: <MainLayout />,
+          children: [
+            {
+              path: path.profile,
+              element: (
+                <Suspense>
+                  <Profile />
+                </Suspense>
+              )
+            }
+          ]
+        }
+      ]
     }
   ])
   return routeElements
