23308110dcd1eaab0b4053a6088c3b0d5ad5525a
nguyentronglinh045 <nguyentronglinh045@gmail.com>
2023-09-22 16:12:53 +0700

    format code

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -16,7 +16,7 @@ const Card = () => {
             <span>Sắp cháy hàng</span>
           </div>
           <div
-            className={`remain absolute left-0 top-0 z-[1] h-full w-3/4
+            className={`remain  absolute left-0 top-0 z-[1] h-full w-3/4
           rounded-xl bg-gradient-to-r from-[#ec1a17] to-[#ffad00]`}
           ></div>
         </div>
@@ -34,10 +34,10 @@ const Card = () => {
             viewBox='0 0 24 24'
             fill='none'
             stroke='white'
-            stroke-width='2'
-            stroke-linecap='round'
-            stroke-linejoin='round'
-            class='lucide lucide-cog myDIV'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            className='lucide lucide-cog myDIV'
           >
             <path d='M12 20a8 8 0 1 0 0-16 8 8 0 0 0 0 16Z' />
             <path d='M12 14a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z' />
