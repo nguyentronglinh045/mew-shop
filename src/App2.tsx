14b6c4cdd0252b20925ef2b2ec6c5a4ac62ac8a2
nguyentronglinh045 <nguyentronglinh045@gmail.com>
2023-09-22 16:57:13 +0700

    test

---
+++ src/App2.tsx
@@ -0,0 +1,23 @@
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import 'react-multi-carousel/lib/styles.css'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { HelmetProvider } from 'react-helmet-async'
+import ErrorBoundary from './components/ErrorBoundary'
+import useRouteElement from './useRouteElement'
+
+function App2() {
+  const routeElements = useRouteElement()
+
+  return (
+    <HelmetProvider>
+      <ErrorBoundary>
+        {routeElements}
+        <ToastContainer />
+      </ErrorBoundary>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </HelmetProvider>
+  )
+}
+
+export default App2
