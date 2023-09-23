353b8305f451c2ab2f33c005da01c8bad4b172d4
nguyentronglinh045 <nguyentronglinh045@gmail.com>
2023-09-23 17:14:46 +0700

    handle refresh-token

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -3,13 +3,18 @@ import { User } from 'src/types/user.type'
 export const setAccessTokenToLS = (accessToken: string) => {
   localStorage.setItem('access_token', accessToken)
 }
+export const setRefreshTokenToLS = (refreshToken: string) => {
+  localStorage.setItem('refresh_token', refreshToken)
+}
 
 export const clearDataFromLS = () => {
   localStorage.removeItem('access_token')
+  localStorage.removeItem('refresh_token')
   localStorage.removeItem('profile')
 }
 
 export const getAccessTokenFromLS = () => localStorage.getItem('access_token') || ''
+export const getRefreshTokenFromLS = () => localStorage.getItem('refresh_token') || ''
 
 export const getProfileFromLS = () => {
   const result = localStorage.getItem('profile')
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosError, HttpStatusCode } from 'axios'
 import { apiKey } from './http'
+import { ErrorResponseApi } from 'src/types/utils.type'
 
 //type predicate function
 export function isAxiosError<T>(error: unknown): error is AxiosError<T> {
@@ -9,6 +10,16 @@ export function isAxiosError<T>(error: unknown): error is AxiosError<T> {
 
 export function isAxiosUnprocessableEntityError<FormError>(error: unknown): error is AxiosError<FormError> {
   return isAxiosError(error) && error.response?.status === HttpStatusCode.UnprocessableEntity
+}
+export function isAxiosUnauthorizedError<UnauthorizedError>(error: unknown): error is AxiosError<UnauthorizedError> {
+  return isAxiosError(error) && error.response?.status === HttpStatusCode.Unauthorized
+}
+
+export function isAxiosExpiredTokenError<UnauthorizedError>(error: unknown): error is AxiosError<UnauthorizedError> {
+  return (
+    isAxiosUnauthorizedError<ErrorResponseApi<{ name: string; message: string }>>(error) &&
+    error.response?.data?.data?.name === 'EXPIRED_TOKEN'
+  )
 }
 
 export function getAvatarURL(avtID: string | undefined) {
