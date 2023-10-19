619f5435dc0d78867032e4ec7f54444cfd6060ac
nguyentronglinh045 <nguyentronglinh045@gmail.com>
2023-10-19 09:31:45 +0700

    test meta tag

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,6 +3,7 @@ import FeaturedCategories from 'src/components/FeaturedCategories'
 import FlashSale from 'src/components/FlashSale'
 import SliderBanner from 'src/components/SliderBanner'
 import TabProducts from 'src/components/TabProduct'
+import Logo from '../../assets/logo/logo_foo.webp'
 
 export default function Home() {
   return (
@@ -10,6 +11,10 @@ export default function Home() {
       <Helmet>
         <title>Trang chủ</title>
         <meta name='description' content='Trang chủ dự án Meo Mobile' />
+        <meta property='og:title' content='Trang chủ dự án Meo Mobile' />
+        <meta property='og:description' content='Dự án cá nhân về ReactJS và Vite' />
+        <meta property='og:image' content={Logo} />
+        <meta property='og:type' content='website' />
       </Helmet>
       <SliderBanner />
       <FeaturedCategories />
